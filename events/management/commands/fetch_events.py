e9b7f43e2ac9a03bd10513bcefa06d5eac7cdcb8
Laurent Peuch <cortex@worlddomination.be>
2013-10-12 17:20:21 +0200

    [enh] also parse neutrinet

--- events/management/commands/fetch_events.py
+++ events/management/commands/fetch_events.py
@@ -9,7 +9,7 @@ from events.models import Event
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for source in [urlab]:
+        for source in [urlab, neutrinet]:
             with transaction.commit_on_success():
                 source()
 
@@ -37,3 +37,24 @@ def urlab():
         print "adding %s [%s] (%s)..." % (title, "urlab", location)
 
 
+def neutrinet():
+    # clean events
+    Event.objects.filter(source="neutrinet").delete()
+
+    soup = BeautifulSoup(urlopen("http://neutrinet.be/index.php?title=Main_Page").read())
+
+    for event in filter(lambda x: x, map(lambda x: x('td'), soup('table', 'wikitable')[0]('tr'))):
+        title = event[0].text
+        url = "https://wiki;urlab.be" + event[0].a["href"]
+        start = parse(event[1].text)
+        location = event[2].text
+
+        Event.objects.create(
+            title=title,
+            source="neutrinet",
+            url=url,
+            start=start,
+            location=location,
+        )
+
+        print "adding %s [%s] (%s)..." % (title, "neutrinet", location)
