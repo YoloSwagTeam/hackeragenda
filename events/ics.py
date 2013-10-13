4d9f6c95fc871ade024b9417c6188cd52d4e397b
Laurent Peuch <cortex@worlddomination.be>
2013-10-14 01:03:41 +0200

    [mod] try to arrange .ics

--- events/ics.py
+++ events/ics.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from datetime import timedelta
+
 from icalendar import Calendar, Event as ICSEvent
+
+from django.http import HttpResponse
 from .models import Event
 
 def get_ics_of_events(request):
@@ -10,9 +13,14 @@ def get_ics_of_events(request):
     for event in Event.objects.all():
         ics_event = ICSEvent()
         ics_event.add('summary', '%s [%s]' % (event.title, event.source))
-        if event.end:
-            ics_event.add('dtstart', event.start)
-            ics_event.add('dtend', event.end)
+        ics_event.add('dtstart', event.start)
+        if event.start.hour == 0 and event.start.minute == 0:
+            ics_event.add('dtstart', event.start.date())
+        else:
+            if event.end:
+                ics_event.add('dtend', event.end)
+            else:
+                ics_event.add('dtend', event.start + timedelta(hours=1))
         ics_event.add('dtstamp', event.start)
         ics_event['uid'] = event.url
 
