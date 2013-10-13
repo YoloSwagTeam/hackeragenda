14c18b4b4ccdbdde29bd9208fcfd0123fd3ac4a8
Laurent Peuch <cortex@worlddomination.be>
2013-10-14 01:36:40 +0200

    [enh] better event rendering to handle start/end time

--- events/views.py
+++ events/views.py
@@ -1,13 +1,29 @@
 import json
+from datetime import timedelta
+
 from django.http import HttpResponse
 
 from .models import Event
 
 def get_events_in_json(request):
-    return HttpResponse(json.dumps([{
-        "title": "%s [%s]" % (x.title, x.source),
-        "start": x.start.strftime("%F"),
-        "color": x.color,
-        "textColor": x.text_color,
-        "url": x.url,
-    } for x in Event.objects.all()]), mimetype="application/json")
+    return HttpResponse(json.dumps(map(event_to_fullcalendar_format, Event.objects.all())), mimetype="application/json")
+
+def event_to_fullcalendar_format(event):
+    to_return = {
+        "title": "%s [%s]" % (event.title, event.source),
+        "color": event.color,
+        "textColor": event.text_color,
+        "url": event.url,
+    }
+
+    if event.start.hour == 0 and event.start.minute == 0:
+        to_return["start"] = event.start.strftime("%F")
+    else:
+        to_return["start"] = event.start.strftime("%F %X")
+        to_return["allDay"] = False
+        if event.end:
+            to_return["end"] = event.end.strftime("%F %X")
+        else:
+            to_return["end"] = (event.start + timedelta(hours=3)).strftime("%F %X")
+
+    return to_return
