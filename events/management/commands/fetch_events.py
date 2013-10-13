a72e7bd2f37bedf5d5858a5da3a91940b93f6ea3
Laurent Peuch <cortex@worlddomination.be>
2013-10-13 17:19:55 +0200

    [enh] add constantvzw \o/

--- events/management/commands/fetch_events.py
+++ events/management/commands/fetch_events.py
@@ -10,7 +10,7 @@ from events.models import Event
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for source in [urlab, neutrinet, hsbxl, agenda_du_libre_be]:
+        for source in [urlab, neutrinet, hsbxl, agenda_du_libre_be, constantvzw]:
             with transaction.commit_on_success():
                 source()
 
@@ -110,3 +110,43 @@ def agenda_du_libre_be():
         )
 
         print "adding %s [%s] (%s)..." % (event.title, "agenda_du_libre_be", event.summary.split(":")[0])
+
+def constantvzw():
+    Event.objects.filter(source="constantvzw").delete()
+
+    soup = BeautifulSoup(urlopen("http://www.constantvzw.org/site/").read())
+
+    for event in soup.find("div", id="flow")("div", recursive=False)[:-1]:
+        title = event('a')[1].text
+        url = "http://www.constantvzw.org/site/" + event('a')[1]["href"]
+
+        if len(event.span.text.split(" @ ")) == 2:
+            time, location = event.span.text.split(" @ ")
+        else:
+            time = event.span.text.split("@")[0].strip()
+            location = None
+
+        if time.startswith("From "):
+            data = time.split(" ")[1:]
+            if len(data) == 4:
+                start = parse("%s %s" % (data[0], data[3]))
+                end = parse("%s %s" % (data[2], data[3]))
+            else:
+                start = parse("%s %s" % (data[0], data[1]))
+                end = parse("%s %s" % (data[3], data[4]))
+        else:
+            start = parse(time).replace(tzinfo=None)
+            end = None
+
+        Event.objects.create(
+            title=title,
+            source="constantvzw",
+            url=url,
+            start=start,
+            end=end,
+            location=location,
+            color="#D2C7BA",
+            text_color="black",
+        )
+
+        print "adding %s [%s] (%s)..." % (title, "constantvzw", location)
