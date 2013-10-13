1388572bb63c2c3baab980b22e1bf211ac420c78
Laurent Peuch <cortex@worlddomination.be>
2013-10-13 21:32:04 +0200

    [doc] add information on the api

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+from events.api import EventResource
+
+event_resource = EventResource()
 
 urlpatterns = patterns('events.views',
     url(r'^events.json$', 'get_events_in_json', name='events_json'),
+    url(r'^api/', include(event_resource.urls), name='events_api'),
 )
