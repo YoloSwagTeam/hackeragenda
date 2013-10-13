b0e0140cd8f158e29db12bde6d92404a757249f8
Laurent Peuch <cortex@worlddomination.be>
2013-10-14 00:53:31 +0200

    [enh] export in ICS \o/

---
+++ events/ics.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from icalendar import Calendar, Event as ICSEvent
+from .models import Event
+
+def get_ics_of_events(request):
+    cal = Calendar()
+    cal.add('prodid', 'Hacker Agenda')
+    cal.add('version', '2.0')
+
+    for event in Event.objects.all():
+        ics_event = ICSEvent()
+        ics_event.add('summary', '%s [%s]' % (event.title, event.source))
+        if event.end:
+            ics_event.add('dtstart', event.start)
+            ics_event.add('dtend', event.end)
+        ics_event.add('dtstamp', event.start)
+        ics_event['uid'] = event.url
+
+        cal.add_component(ics_event)
+
+    return HttpResponse(cal.to_ical())
--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url, include
 from events.api import EventResource
 from .feeds import NextEventsFeed
+from .ics import get_ics_of_events
 
 event_resource = EventResource()
 
 urlpatterns = patterns('events.views',
     url(r'^events.json$', 'get_events_in_json', name='events_json'),
     url(r'^events.rss$', NextEventsFeed(), name='events_rss'),
+    url(r'^events.ics$', get_ics_of_events, name='events_ics'),
     url(r'^api/', include(event_resource.urls), name='events_api'),
 )
