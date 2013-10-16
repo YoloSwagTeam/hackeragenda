53d74cb9dbc35480261cb61f8adeb318284e8436
iTitou <titouanchristophe@gmail.com>
2013-10-16 22:04:10 +0200

    [enh] Use config file for event colors

---
+++ events/colors.py
@@ -0,0 +1,12 @@
+COLORS = {
+	"urlab": {"bg": "pink", "fg": "black"},
+	"neutrinet": {"bg": "DarkBlue", "fg": "white"},
+	"hsbxl": {"bg": "coral", "fg": "white"},
+	"agenda_du_libre_be": {"bg": "black", "fg": "white"},
+	"constantvzw": {"bg": "#D2C7BA", "fg": "black"},
+	"bhackspace": {"bg": "DarkGoldenRod", "fg": "white"},
+	"incubhacker": {"bg": "#296038", "fg": "#6FCE91"},
+	"opengarage": {"bg": "DarkOrchid", "fg": "white"},
+	"whitespace": {"bg": "white", "fg": "black"},
+	"voidwarranties": {"bg": "#25272C", "fg": "#C58723"}
+}
