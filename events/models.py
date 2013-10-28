e533e67d6b5508282c2a899789683de6cc7d1b68
iTitou <titouanchristophe@gmail.com>
2013-10-28 22:09:34 +0100

    [ref] Use @properties for colors

--- events/models.py
+++ events/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from events.colors import COLORS
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
@@ -15,3 +15,15 @@ class Event(models.Model):
 
     def __unicode__(self):
         return "%s [%s]" % (self.title, self.source)
+
+    @property
+    def background_color(self):
+        if self.source in COLORS:
+            return COLORS[self.source]['bg']
+        return None
+
+    @property
+    def text_color(self):
+        if self.source in COLORS:
+            return COLORS[self.source]['fg']
+        return None
--- events/views.py
+++ events/views.py
@@ -9,24 +9,14 @@ from .colors import COLORS
 
 class EventListView(ListView):
     template_name = "home.haml"
-    
     queryset = Event.objects.filter(start__gte=datetime.now).order_by("start")
     
-    def get_context_data(self, **kwargs):
-        context = super(EventListView, self).get_context_data(**kwargs)
-        for event in context['event_list']:
-            event.text_color = COLORS[event.source]['fg']
-            event.color = COLORS[event.source]['bg']
-        return context
-
 def get_events_in_json(request):
     return HttpResponse(json.dumps(map(event_to_fullcalendar_format, Event.objects.all())), mimetype="application/json")
 
 def event_to_fullcalendar_format(event):
     to_return = {
         "title": "%s [%s]" % (event.title, event.source),
-        "color": COLORS[event.source]['bg'],
-        "textColor": COLORS[event.source]['fg'],
         "url": event.url,
     }
 
