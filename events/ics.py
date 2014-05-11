c0d9bf2d496a3950438f925bb92b9cd2e1ba00c6
Laurent Peuch <cortex@worlddomination.be>
2014-05-11 08:17:42 +0200

    [enh] allows ics to be filtered by events if passed by GET parameters

--- events/ics.py
+++ events/ics.py
@@ -4,6 +4,7 @@ from icalendar import Calendar, Event as ICSEvent
 
 from django.http import HttpResponse
 from .models import Event
+from .utils import filter_events
 
 
 def get_ics_of_events(request):
@@ -11,7 +12,7 @@ def get_ics_of_events(request):
     cal.add('prodid', 'Hacker Agenda')
     cal.add('version', '2.0')
 
-    for event in Event.objects.all():
+    for event in filter_events(request=request, queryset=Event.objects.all()):
         ics_event = ICSEvent()
         ics_event.add('summary', '%s [%s]' % (event.title, event.source))
         if event.start.hour == 0 and event.start.minute == 0:
