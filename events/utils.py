fa816675c5f52afe9cce5e18fcfb076a577ccfa7
Laurent Peuch <cortex@worlddomination.be>
2014-05-12 07:12:23 +0200

    [enh] also pass section parameter to rss and ics button for futer updates

--- events/utils.py
+++ events/utils.py
@@ -1,14 +1,28 @@
+from django.conf import settings
+
 def filter_events(request, queryset):
-    if request.GET.getlist("source"):
-        queryset = queryset.filter(source__in=request.GET.getlist("source"))
+    section = request.GET.get("section")
+    if section:
+        source = settings.PREDEFINED_FILTERS[section]["source"]
+        exclude_source = settings.PREDEFINED_FILTERS[section]["exclude_source"]
+        tag = settings.PREDEFINED_FILTERS[section]["tag"]
+        exclude_tag = settings.PREDEFINED_FILTERS[section]["exclude_tag"]
+    else:
+        source = request.GET.getlist("source")
+        exclude_source = request.GET.getlist("exclude_source")
+        tag = request.GET.getlist("tag")
+        exclude_tag = request.GET.getlist("exclude_tag")
 
-    if request.GET.getlist("exclude_source"):
-        queryset = queryset.exclude(source__in=request.GET.getlist("exclude_source"))
+    if source:
+        queryset = queryset.filter(source__in=source)
 
-    if request.GET.getlist("tag"):
-        queryset = queryset.filter(tags__name__in=request.GET.getlist("tag"))
+    if exclude_source:
+        queryset = queryset.exclude(source__in=exclude_source)
 
-    if request.GET.getlist("exclude_tag"):
-        queryset = queryset.exclude(tags__name__in=request.GET.getlist("exclude_tag"))
+    if tag:
+        queryset = queryset.filter(tags__name__in=tag)
+
+    if exclude_tag:
+        queryset = queryset.exclude(tags__name__in=exclude_tag)
 
     return queryset
