492aa962a788156e064d835c11eb075bcd5a1837
Laurent Peuch <cortex@worlddomination.be>
2014-05-15 13:27:05 +0200

    [enh] actually make the command tweet

--- events/management/commands/tweet.py
+++ events/management/commands/tweet.py
@@ -1,7 +1,14 @@
 import re
+import sys
+import time
+import tweepy
+
+from datetime import date, timedelta
+from optparse import make_option
 
 from django.core.management.base import BaseCommand
-from datetime import date, timedelta
+from django.conf import settings
+
 from events.models import Event
 
 
@@ -9,10 +16,41 @@ def tweet_size(tweet):
     return len(re.sub("https://\S*", "X"*23, re.sub("http://\S*", "X"*22, tweet)))
 
 
+def connect_to_twitter():
+    if not hasattr(settings, "TWITTER_ACCESS_TOKEN") or not hasattr(settings, "TWITTER_ACCESS_SECRET"):
+        print "Error: TWITTER_ACCESS_TOKEN and TWITTER_ACCESS_SECRET must be set in the settings.py"
+        sys.exit(1)
+
+    auth = tweepy.OAuthHandler("KwayVtavdRYPKmnSuZO2w", "vvhA31BhosJdcpjmQJk8ORqp7M0whIDBlIAGqwAQ")
+    auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_SECRET)
+    return tweepy.API(auth)
+
+
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+       make_option('--run',
+            action='store_true',
+            dest='run',
+            default=False,
+            help='Actually tweet'),
+    )
+
     def handle(self, *args, **options):
-        for tweet in self.generate_tweets():
-            print tweet
+        if not options["run"]:
+            print "Warning: won't tweet, use --run to actually tweet."
+        else:
+            self.twitter = connect_to_twitter()
+
+        for tweet in list(self.generate_tweets())[1:]:
+            if not options["run"]:
+                print tweet
+            else:
+                self.tweet(tweet)
+
+    def tweet(self, t):
+        print "Tweet:", t
+        self.twitter.update_status(t)
+        time.sleep(10)
 
     def generate_tweets(self):
         today_events = Event.objects.filter(start__gte=date.today()).filter(start__lt=date.today() + timedelta(days=1))
@@ -21,7 +59,7 @@ class Command(BaseCommand):
         for tweet in self.format_tweets(today_events):
             yield tweet
 
-        if date.today().weekday() == 3:
+        if date.today().weekday() == 0:
             for tweet in self.format_tweets(this_week_other_events):
                 yield tweet
 
@@ -37,7 +75,7 @@ class Command(BaseCommand):
             tweet = [format_title(event), event.url, "#" + "".join(event.source.replace("_", " ").title().split())]
             if tweet_size(" ".join(tweet)) > 141:
                 to_remove = tweet_size(" ".join(tweet)) - 140
-                title = event.title[to_remove:]
+                title = event.title[to_remove + 3:] + "..."
                 tweet[0] = format_title(event)
 
             tweet = " ".join(tweet)
