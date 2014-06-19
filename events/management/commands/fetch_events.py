949cd1f3e1f57bb1c81c13c9f88348295a3d6e39
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-06-19 17:48:22 +0200

    [vg_be] add 'eva' parser

--- events/management/commands/fetch_events.py
+++ events/management/commands/fetch_events.py
@@ -994,3 +994,72 @@ def wolfplex(create_event):
         )
 
         db_event.tags.add("hackerspace")
+
+@event_source(background_color="#539316", text_color="#FFFFFF", agenda="vg_be", url="http://www.evavzw.be")
+def eva(create_event):
+    """EVA werd opgericht in 2000 door een handjevol gemotiveerde mensen uit het Gentse en is sindsdien uitgegroeid tot een organisatie met een tiental vaste medewerkers, verschillende lokale groepen en honderden vrijwilligers. Sinds haar ontstaan heeft de organisatie al heel wat activiteiten en projecten op poten gezet."""
+    nl_months = {
+             'januari': 1,
+             'februari': 2,
+             'maart': 3,
+             'april': 4,
+             'mei': 5,
+             'juni': 6,
+             'juli': 7,
+             'augustus': 8,
+             'september': 9,
+             'oktober': 10,
+             'november': 11,
+             'december': 12
+             }
+
+    tags_mapping = {
+            'EVA kookcursus': 'cooking class',
+            'kookcursus': 'cooking class',
+            'eten': 'eating',
+            'EVA eten': 'eating',
+            }
+
+    now = datetime.now()
+
+    src_url = "http://www.evavzw.be/index.php?option=com_agenda"
+    soup = BeautifulSoup(requests.get(src_url).content)
+    for month in soup(attrs={"class":'agenda'}):
+        month_nb = int(nl_months[month['id'].split('_')[1]])
+
+        # If the month has passed assume it's next year
+        if month_nb < now.month:
+            year = now.year + 1
+        else:
+            year = now.year
+
+        for day in month('li'):
+            if day.get('id') is None:
+                continue
+
+            day_nb = int(day['id'].split('_')[1])
+            for event in day('li'):
+                title = event.a.text
+                url = 'http://www.evavzw.be/' + event.a['href']
+                hour, minute = 0, 0
+                tags = []
+
+                for span in event('span'):
+                    if 'time' in span['class']:
+                        hour, minute = map(int, span.text.split('vanaf ')[1].split('u'))
+                    else:
+                        # Tag
+                        tag = tags_mapping.get(span.text)
+                        if tag is not None:
+                            tags.append(tag)
+
+                start = datetime(year, month_nb, day_nb, hour, minute)
+
+                event = create_event(
+                    title=title,
+                    url=url,
+                    start=start,
+                )
+
+                for tag in tags:
+                    event.tags.add(tag)
