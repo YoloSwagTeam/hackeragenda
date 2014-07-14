569b6f6a3a5218eccd05b8f2088377664cb33116
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-07-14 22:53:00 +0200

    [vg_be] sort parsers alphabetically

--- events/management/commands/fetch_events.py
+++ events/management/commands/fetch_events.py
@@ -300,6 +300,9 @@ generic_meetup("belgian_puppet_user_group", "Belgian-Puppet-User-Group", backgro
 generic_meetup("bescala", "BeScala", background_color="#FEE63C", text_color="#000000", agenda="be", tags=["java", "scala", "jvm", "programming"], description="<p>The Belgian Scala User Group.</p>")
 
 
+generic_facebook("bite_back", "BiteBackOrg", background_color="#db0c38", text_color="#FFFFFF", agenda="vg_be", tags=["animal-rights"])
+
+
 @event_source(background_color="DarkGoldenRod", text_color="white", agenda="be", url="http://bhackspace.be")
 def bhackspace(create_event):
     "<p>The BHackspace is a hackerspace located in Bastogne, Belgium.</p>"
@@ -496,6 +499,76 @@ def electrolab(create_event):
 generic_meetup("ember_js_brussels", "Ember-js-Brussels", background_color="#FC745D", text_color="white", agenda="be", tags=["emberjs", "javascript", "programming", "webdev"], description="This is a group for anyone interested in developing web applications in Ember.js. I created this group because it's nice to have a local community for sharing knowledge, ideas and inspiration about this awesome web framework. The learning curve for Ember.js is not the lightest so it will also be a place to share your frustrations! Beginner or expert, everyone is welcome.")
 
 
+@event_source(background_color="#539316", text_color="#FFFFFF", agenda="vg_be", url="http://www.evavzw.be")
+def eva(create_event):
+    """EVA werd opgericht in 2000 door een handjevol gemotiveerde mensen uit het Gentse en is sindsdien uitgegroeid tot een organisatie met een tiental vaste medewerkers, verschillende lokale groepen en honderden vrijwilligers. Sinds haar ontstaan heeft de organisatie al heel wat activiteiten en projecten op poten gezet."""
+    nl_months = {
+             'januari': 1,
+             'februari': 2,
+             'maart': 3,
+             'april': 4,
+             'mei': 5,
+             'juni': 6,
+             'juli': 7,
+             'augustus': 8,
+             'september': 9,
+             'oktober': 10,
+             'november': 11,
+             'december': 12
+             }
+
+    tags_mapping = {
+            'EVA kookcursus': 'cooking class',
+            'kookcursus': 'cooking class',
+            'eten': 'eating',
+            'EVA eten': 'eating',
+            }
+
+    now = datetime.now()
+
+    src_url = "http://www.evavzw.be/index.php?option=com_agenda"
+    soup = BeautifulSoup(requests.get(src_url).content)
+    for month in soup(attrs={"class":'agenda'}):
+        month_nb = int(nl_months[month['id'].split('_')[1]])
+
+        # If the month has passed assume it's next year
+        if month_nb < now.month:
+            year = now.year + 1
+        else:
+            year = now.year
+
+        for day in month('li'):
+            if day.get('id') is None:
+                continue
+
+            day_nb = int(day['id'].split('_')[1])
+            for event in day('li'):
+                title = event.a.text
+                url = 'http://www.evavzw.be/' + event.a['href']
+                hour, minute = 0, 0
+                tags = []
+
+                for span in event('span'):
+                    if 'time' in span['class']:
+                        hour, minute = map(int, span.text.split('vanaf ')[1].split('u'))
+                    else:
+                        # Tag
+                        tag = tags_mapping.get(span.text)
+                        if tag is not None:
+                            tags.append(tag)
+
+                start = datetime(year, month_nb, day_nb, hour, minute)
+
+                event = create_event(
+                    title=title,
+                    url=url,
+                    start=start,
+                )
+
+                for tag in tags:
+                    event.tags.add(tag)
+
+
 @event_source(background_color="#C9C4BF", text_color="black", key=None, agenda="be", url="http://fo.am")
 def foam(create_event):
     """
@@ -535,6 +608,9 @@ def foam(create_event):
 
         if "FoAM Apéro" in event.title:
             event.tags.add("meeting")
+
+
+generic_facebook("gaia", "gaia.be", background_color="#fdfafa", text_color="#5d3b80", agenda="vg_be", tags=["animal-rights"])
 
 
 @event_source(background_color="coral", text_color="white", key=None, agenda="be", url="https://hackerspace.be")
@@ -595,6 +671,52 @@ def incubhacker(create_event):
             event.tags.add("meeting")
 
         event.tags.add("hackerspace")
+
+
+@event_source(background_color="#66b822", text_color="#FFFFFF", agenda="vg_be", url="http://www.jeudiveggie.be")
+def jeudi_veggie(create_event):
+    """Le Jeudi Veggie est une campagne qui nous invite à découvrir un jour par semaine, une assiette plus équilibrée, qui fait la part belle aux céréales, aux fruits et aux légumes. Une assiette sans viande ni poisson, mais avec plein de fruits et légumes."""
+    tags_mapping = {
+            'cours de cuisine': 'cooking class',
+            'diner': 'eating',
+            }
+
+    # request events from today to next year
+    today = datetime.now().strftime("%d-%m-%Y")
+    next_year = (datetime.now() + timedelta(weeks=52)).strftime("%d-%m-%Y")
+
+    src_url = "http://www.jeudiveggie.be/kalender/zoeken/%s_%s/" % (today, next_year)
+    soup = BeautifulSoup(requests.get(src_url).content)
+
+    main = soup.find(id='itemtag')
+    for d in main(attrs={'class': 'date'}):
+        full_date = d['href'].split('/')[2]
+        day, month, year = full_date.split('-')
+
+        url = 'http://www.jeudiveggie.be' + d['href']
+        if d.text[2] == ':':
+            time, title = d.text.split(' ', 1)
+            hour, minute = time.split(':')
+
+            start = datetime(int(year), int(month), int(day), int(hour), int(minute))
+        else:
+            title = d.text
+            start = datetime(int(year), int(month), int(day))
+
+        event = create_event(
+            title=title,
+            url=url,
+            start=start,
+        )
+
+        # tags
+        span = d.findNextSibling()
+        if span.name == 'span':
+            tags = span.text[1:-1].split(',')
+            for tag in map(unicode.strip, tags):
+                t = tags_mapping.get(tag)
+                if t is not None:
+                    event.tags.add(t)
 
 
 generic_meetup("laravel_brussels", "Laravel-Brussels", background_color="#FFFFFF", text_color="#FB503B", agenda="be", tags=["bruxelles", "laravel", "php", "webdev", "programming"], description='<p>A group for anyone interested in learning about and sharing knowledge on Laravel, the "PHP framework for web artisans". The group welcomes beginners and experts, amateurs and pros, young and old, etc. Laravel is an accessible, yet powerful framework for web application development. Its expressive, elegant syntax and its clean structure make PHP development a real joy. As the Laravel community keeps growing, this group is an attempt to get Belgium-based users to know each other, and to spread the word!</p>')
@@ -994,120 +1116,3 @@ def wolfplex(create_event):
         )
 
         db_event.tags.add("hackerspace")
-
-@event_source(background_color="#539316", text_color="#FFFFFF", agenda="vg_be", url="http://www.evavzw.be")
-def eva(create_event):
-    """EVA werd opgericht in 2000 door een handjevol gemotiveerde mensen uit het Gentse en is sindsdien uitgegroeid tot een organisatie met een tiental vaste medewerkers, verschillende lokale groepen en honderden vrijwilligers. Sinds haar ontstaan heeft de organisatie al heel wat activiteiten en projecten op poten gezet."""
-    nl_months = {
-             'januari': 1,
-             'februari': 2,
-             'maart': 3,
-             'april': 4,
-             'mei': 5,
-             'juni': 6,
-             'juli': 7,
-             'augustus': 8,
-             'september': 9,
-             'oktober': 10,
-             'november': 11,
-             'december': 12
-             }
-
-    tags_mapping = {
-            'EVA kookcursus': 'cooking class',
-            'kookcursus': 'cooking class',
-            'eten': 'eating',
-            'EVA eten': 'eating',
-            }
-
-    now = datetime.now()
-
-    src_url = "http://www.evavzw.be/index.php?option=com_agenda"
-    soup = BeautifulSoup(requests.get(src_url).content)
-    for month in soup(attrs={"class":'agenda'}):
-        month_nb = int(nl_months[month['id'].split('_')[1]])
-
-        # If the month has passed assume it's next year
-        if month_nb < now.month:
-            year = now.year + 1
-        else:
-            year = now.year
-
-        for day in month('li'):
-            if day.get('id') is None:
-                continue
-
-            day_nb = int(day['id'].split('_')[1])
-            for event in day('li'):
-                title = event.a.text
-                url = 'http://www.evavzw.be/' + event.a['href']
-                hour, minute = 0, 0
-                tags = []
-
-                for span in event('span'):
-                    if 'time' in span['class']:
-                        hour, minute = map(int, span.text.split('vanaf ')[1].split('u'))
-                    else:
-                        # Tag
-                        tag = tags_mapping.get(span.text)
-                        if tag is not None:
-                            tags.append(tag)
-
-                start = datetime(year, month_nb, day_nb, hour, minute)
-
-                event = create_event(
-                    title=title,
-                    url=url,
-                    start=start,
-                )
-
-                for tag in tags:
-                    event.tags.add(tag)
-
-@event_source(background_color="#66b822", text_color="#FFFFFF", agenda="vg_be", url="http://www.jeudiveggie.be")
-def jeudi_veggie(create_event):
-    """Le Jeudi Veggie est une campagne qui nous invite à découvrir un jour par semaine, une assiette plus équilibrée, qui fait la part belle aux céréales, aux fruits et aux légumes. Une assiette sans viande ni poisson, mais avec plein de fruits et légumes."""
-    tags_mapping = {
-            'cours de cuisine': 'cooking class',
-            'diner': 'eating',
-            }
-
-    # request events from today to next year
-    today = datetime.now().strftime("%d-%m-%Y")
-    next_year = (datetime.now() + timedelta(weeks=52)).strftime("%d-%m-%Y")
-
-    src_url = "http://www.jeudiveggie.be/kalender/zoeken/%s_%s/" % (today, next_year)
-    soup = BeautifulSoup(requests.get(src_url).content)
-
-    main = soup.find(id='itemtag')
-    for d in main(attrs={'class': 'date'}):
-        full_date = d['href'].split('/')[2]
-        day, month, year = full_date.split('-')
-
-        url = 'http://www.jeudiveggie.be' + d['href']
-        if d.text[2] == ':':
-            time, title = d.text.split(' ', 1)
-            hour, minute = time.split(':')
-
-            start = datetime(int(year), int(month), int(day), int(hour), int(minute))
-        else:
-            title = d.text
-            start = datetime(int(year), int(month), int(day))
-
-        event = create_event(
-            title=title,
-            url=url,
-            start=start,
-        )
-
-        # tags
-        span = d.findNextSibling()
-        if span.name == 'span':
-            tags = span.text[1:-1].split(',')
-            for tag in map(unicode.strip, tags):
-                t = tags_mapping.get(tag)
-                if t is not None:
-                    event.tags.add(t)
-
-generic_facebook("bite_back", "BiteBackOrg", background_color="#db0c38", text_color="#FFFFFF", agenda="vg_be", tags=["animal-rights"])
-generic_facebook("gaia", "gaia.be", background_color="#fdfafa", text_color="#5d3b80", agenda="vg_be", tags=["animal-rights"])
