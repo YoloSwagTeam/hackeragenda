27724e16b1e328130dd3529604f774a84a2c5863
Laurent Peuch <cortex@worlddomination.be>
2014-08-31 12:44:33 +0200

    [enh] key can be a callable to avoid duplicated events

--- events/management/commands/fetch_events.py
+++ events/management/commands/fetch_events.py
@@ -79,7 +79,10 @@ def event_source(background_color, text_color, url, agenda=None, key="url", desc
     def event_source_wrapper(func, org_name=None):
         def fetch_events(quiet):
             def create_event(**detail):
-                if key not in (None, False) and Event.objects.filter(**{key: detail[key]}):
+                if callable(key):
+                    key(event_model=Event, detail=detail)
+
+                elif key not in (None, False) and Event.objects.filter(**{key: detail[key]}):
                     Event.objects.filter(**{key: detail[key]}).delete()
 
                 res = Event.objects.create(source=org_name, text_color=SOURCES_OPTIONS[org_name]["fg"], border_color=SOURCES_OPTIONS[org_name]["bg"], agenda=agenda, **detail)
