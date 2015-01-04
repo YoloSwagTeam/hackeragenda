5a2a8f8a2f57cff63109197139ed6f30066ae633
Laurent Peuch <cortex@worlddomination.be>
2015-01-05 00:24:21 +0100

    [mod] move to django buildin migrations from south

---
+++ events/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import taggit.managers
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taggit', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Event',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('source', models.CharField(max_length=255)),
+                ('color', models.CharField(max_length=255, null=True, blank=True)),
+                ('text_color', models.CharField(max_length=255, null=True, blank=True)),
+                ('border_color', models.CharField(max_length=255, null=True, blank=True)),
+                ('url', models.URLField()),
+                ('all_day', models.BooleanField(default=False)),
+                ('start', models.DateTimeField()),
+                ('end', models.DateTimeField(null=True, blank=True)),
+                ('location', models.CharField(max_length=255, null=True, blank=True)),
+                ('agenda', models.CharField(max_length=255)),
+                ('lon', models.FloatField(default=None, null=True, blank=True)),
+                ('lat', models.FloatField(default=None, null=True, blank=True)),
+                ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
