7ce12f1e37ef646ce24ced55fde6281bd12a8656
Laurent Peuch <cortex@worlddomination.be>
2015-01-05 18:45:42 +0100

    [mod] since it's so slow, using atomic doesn't make any sens here

--- events/management/commands/put_locations_to_every_events.py
+++ events/management/commands/put_locations_to_every_events.py
@@ -20,42 +20,41 @@ class Command(BaseCommand):
         fails = json.load(open("/tmp/hackeragenda_geopy_fails.json", "r")) if os.path.exists("/tmp/fails.json") else []
 
         geolocator = Nominatim()
-        with transaction.atomic():
-            for i in filter(lambda x: x.location.strip(), Event.objects.filter(location__isnull=False, lon__isnull=True, lat__isnull=True)):
-                if i.location in fails:
-                    continue
+        for i in filter(lambda x: x.location.strip(), Event.objects.filter(location__isnull=False, lon__isnull=True, lat__isnull=True)):
+            if i.location in fails:
+                continue
 
-                print i.title, "----", i.location, [i.lon], [i.lat]
+            print i.title, "----", i.location, [i.lon], [i.lat]
 
-                if i.location in caching:
-                    location = caching[i.location]
-                else:
+            if i.location in caching:
+                location = caching[i.location]
+            else:
+                time.sleep(5)
+                try:
+                    location = geolocator.geocode(i.location)
+                except GeocoderTimedOut:
+                    location = None
+
+                if location is None and re.search("\(.*\)", i.location):
+                    print "fail, try to look for:", re.search("(\(.+\))", i.location).group()[1:-1]
                     time.sleep(5)
                     try:
-                        location = geolocator.geocode(i.location)
+                        location = geolocator.geocode(re.search("(\(.+\))", i.location).group()[1:-1])
                     except GeocoderTimedOut:
                         location = None
 
-                    if location is None and re.search("\(.*\)", i.location):
-                        print "fail, try to look for:", re.search("(\(.+\))", i.location).group()[1:-1]
-                        time.sleep(5)
-                        try:
-                            location = geolocator.geocode(re.search("(\(.+\))", i.location).group()[1:-1])
-                        except GeocoderTimedOut:
-                            location = None
+            caching[i.location] = location
 
-                caching[i.location] = location
+            if location is None:
+                fails.append(i.location)
 
-                if location is None:
-                    fails.append(i.location)
+                with open("/tmp/hackeragenda_geopy_fails.json", "w") as save_fails:
+                    save_fails.write(json.dumps(fails, indent=4))
 
-                    with open("/tmp/hackeragenda_geopy_fails.json", "w") as save_fails:
-                        save_fails.write(json.dumps(fails, indent=4))
-
-                    print "fail, continue"
-                    continue
+                print "fail, continue"
+                continue
 
 
-                i.lon = location.longitude
-                i.lat = location.latitude
-                i.save()
+            i.lon = location.longitude
+            i.lat = location.latitude
+            i.save()
