7c9297804761008e0b6ec5b0aafb63b90032a916
Laurent Peuch <cortex@worlddomination.be>
2015-01-07 02:09:34 +0100

    [enh] new command to update location caches fixtures with master data

---
+++ events/management/commands/update_location_cache_fixtures.py
@@ -0,0 +1,10 @@
+import os
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    args = '<poll_id poll_id ...>'
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        os.system("curl http://hackeragenda.be/events/location_cache.yaml > events/fixtures/location_cache.yaml")
