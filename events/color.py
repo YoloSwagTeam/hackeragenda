576f4b905f5d52f799934e0f46e7c0e73c762335
Laurent Peuch <cortex@worlddomination.be>
2015-01-08 01:44:46 +0100

    [mod] pep8

--- events/color.py
+++ events/color.py
@@ -6,16 +6,18 @@ colors_lookup = {
     "darkorchid": (.6, .196, .8)
 }
 
+
 def format_rgba_for_css(r, g, b, a):
-    return 'rgba(%d,%d,%d,%s)' % (255*r, 255*g, 255*b, str(a))
+    return 'rgba(%d,%d,%d,%s)' % (255 * r, 255 * g, 255 * b, str(a))
+
 
 def add_alpha(color, a=1):
     assert type(color) in (unicode, str)
     if color[0] == "#":
         if len(color[1:]) == 3:
-            r, g, b = tuple(int(x, 16)/15. for x in color[1:])
+            r, g, b = tuple(int(x, 16) / 15. for x in color[1:])
         elif len(color[1:]) == 6:
-            r, g, b = tuple(int(color[i:i+2], 16)/255. for i in [1, 3, 5])
+            r, g, b = tuple(int(color[i:i + 2], 16) / 255. for i in [1, 3, 5])
     else:
         r, g, b = colors_lookup[color.lower()]
     return format_rgba_for_css(r, g, b, a)
