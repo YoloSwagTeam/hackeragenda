f268c064d11136294852018cf1d31c80cce171d1
Laurent Peuch <cortex@worlddomination.be>
2015-04-06 22:12:34 +0200

    [mod] migrate dashboard to a CreateView for events

--- administration/urls.py
+++ administration/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
+from .views import CreateEvent
 
 urlpatterns = patterns('administration.views',
-    url(r'^$', 'dashboard', name='administration_dashboard'),
+    url(r'^$', login_required(CreateEvent.as_view()), name='administration_dashboard'),
 )
--- administration/views.py
+++ administration/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+
+from events.models import Event
 
 
-@login_required
-def dashboard(request):
-    return render(request, "administration/dashboard.haml", {})
+class CreateEvent(CreateView):
+    model = Event
+    template_name = "administration/dashboard.haml"
