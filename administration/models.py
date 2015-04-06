d3eb5be5318cfb25d8c0918af789f74651eec614
Laurent Peuch <cortex@worlddomination.be>
2015-04-06 19:50:57 +0200

    [fix] a user can administrate several sources

--- administration/models.py
+++ administration/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 
 class UserSource(models.Model):
     source = models.CharField(max_length=255)
-    user = models.OneToOneField(User)
+    user = models.ForeignKey(User)
