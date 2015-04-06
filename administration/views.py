6efc863380c617b9dac0100345a6d469c4d020f5
Laurent Peuch <cortex@worlddomination.be>
2015-04-06 19:17:54 +0200

    [enh] protect administration dashboard from unauthentified users

--- administration/views.py
+++ administration/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def dashboard(request):
     return render(request, "administration/dashboard.haml", {})
