cffcf51a7e5eb743f3fdd8773b0198c994d03c53
Laurent Peuch <cortex@worlddomination.be>
2015-04-06 18:19:34 +0200

    [mod] remove useless code

--- hackeragenda/urls.py
+++ hackeragenda/urls.py
@@ -7,7 +7,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
-    # url(r'^hackeragenda/', include('hackeragenda.foo.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^events/', include('events.urls')),
 )
