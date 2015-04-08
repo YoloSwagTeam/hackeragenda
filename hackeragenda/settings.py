fa0df80dfaee6c8c661ed97325e9233abef7b0eb
Laurent Peuch <cortex@worlddomination.be>
2015-04-08 12:10:39 +0200

    [fix] I need the message framework for the admin interface

--- hackeragenda/settings.py
+++ hackeragenda/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    #'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
