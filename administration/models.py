90d9e75535fed082693f4a85bb3094b33c9564c1
Laurent Peuch <cortex@worlddomination.be>
2015-04-08 12:12:21 +0200

    [enh] add dunderscore unicodes to UserSource and UserAgenda

--- administration/models.py
+++ administration/models.py
@@ -6,7 +6,13 @@ class UserSource(models.Model):
     source = models.CharField(max_length=255)
     user = models.ForeignKey(User)
 
+    def __unicode__(self):
+        return "%s - %s" % (self.user, self.source)
+
 
 class UserAgenda(models.Model):
     agenda = models.CharField(max_length=255)
     user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return "%s - %s" % (self.user, self.agenda)
