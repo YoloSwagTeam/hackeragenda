8c730f2d5482d69a5b8bce84cd5e47ff7e4efc8c
Laurent Peuch <cortex@worlddomination.be>
2015-04-08 12:06:03 +0200

    [enh] add UserAgenda in the admin interface

--- administration/admin.py
+++ administration/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import UserSource
+from .models import UserSource, UserAgenda
 
 
 class UserSourceAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(UserSource, UserSourceAdmin)
+
+
+class UserAgendaAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserAgenda, UserAgendaAdmin)
