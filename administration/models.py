6e6aa75af318928ccc4aae33c4e68bc5c7661c56
Laurent Peuch <cortex@worlddomination.be>
2015-04-08 10:07:09 +0200

    [enh] add relation between user and agenda

--- administration/models.py
+++ administration/models.py
@@ -5,3 +5,8 @@ from django.contrib.auth.models import User
 class UserSource(models.Model):
     source = models.CharField(max_length=255)
     user = models.ForeignKey(User)
+
+
+class UserAgenda(models.Model):
+    agenda = models.CharField(max_length=255)
+    user = models.ForeignKey(User)
