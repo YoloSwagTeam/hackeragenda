038cbaea81f20271dde372d2dee58ba6f39ce6dd
Laurent Peuch <cortex@worlddomination.be>
2015-04-09 12:44:13 +0200

    [fix] port access right control to new model

--- administration/utils.py
+++ administration/utils.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
 
+from .models import Source
+
 
 def user_can_add_events(view):
     def test(user):
-        return user.is_authenticated() and user.usersource_set.exists() and user.useragenda_set.filter(agenda=settings.AGENDA).exists()
+        return user.is_authenticated() and Source.objects.filter(users=user, agenda=settings.AGENDA).exists()
 
     return user_passes_test(test)(view)
