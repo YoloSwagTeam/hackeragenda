ab3563ce70f7e9e2851eb4b182a531226a117fd8
Laurent Peuch <cortex@worlddomination.be>
2015-04-11 18:49:34 +0200

    [enh] add generic view to modify event

--- administration/urls.py
+++ administration/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from django_view_dispatch import dispatch
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import DeleteView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 
 from events.models import Event
@@ -12,5 +12,6 @@ from .utils import user_can_add_events
 
 urlpatterns = patterns('administration.views',
     url(r'^$', user_can_add_events(dispatch(get=views.dashboard, post=views.add_event)), name='administration_dashboard'),
+    url(r'^event/(?P<pk>\d+)/update/$', user_can_add_events(UpdateView.as_view(model=Event, template_name="administration/event_form.haml", success_url=reverse_lazy('administration_dashboard'))), name='administration_event_update'),
     url(r'^event/(?P<pk>\d+)/delete/$', user_can_add_events(DeleteView.as_view(model=Event, template_name="administration/event_confirm_delete.haml", success_url=reverse_lazy('administration_dashboard'))), name='administration_event_delete'),
 )
