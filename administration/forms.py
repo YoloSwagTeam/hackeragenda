3dace7a7d83d30f393600a373e2eab6c31a1923e
Laurent Peuch <cortex@worlddomination.be>
2015-04-11 19:56:25 +0200

    [enh] make Datetime form field support datetime picker format

--- administration/forms.py
+++ administration/forms.py
@@ -4,12 +4,20 @@ from django.conf import settings
 from .models import Source
 
 
+input_formats = list(forms.DateTimeField.input_formats) + [
+    '%Y/%m/%d %H:%M:%S',
+    '%Y/%m/%d %H:%M',
+    '%Y/%m/%d %Hh%M',
+    '%Y/%m/%d %Hh',
+    '%Y/%m/%d',
+]
+
 class AddEventForm(forms.Form):
     title = forms.CharField()
     source = forms.ModelChoiceField(queryset=Source.objects.all(), empty_label=None)
     url = forms.URLField()
-    start = forms.DateTimeField()
-    end = forms.DateTimeField(required=False)
+    start = forms.DateTimeField(input_formats=input_formats)
+    end = forms.DateTimeField(required=False, input_formats=input_formats)
     all_day = forms.BooleanField(required=False)
     location = forms.CharField(required=False)
 
