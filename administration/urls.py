084c3fd9879a7f5c985d67ee93da1118185d0e08
Laurent Peuch <cortex@worlddomination.be>
2015-04-11 18:14:11 +0200

    [enh] add deleting generic view for events

--- administration/urls.py
+++ administration/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, url
 from django_view_dispatch import dispatch
+from django.views.generic.edit import DeleteView
+
+from events.models import Event
 
 from . import views
 
@@ -8,4 +11,5 @@ from .utils import user_can_add_events
 
 urlpatterns = patterns('administration.views',
     url(r'^$', user_can_add_events(dispatch(get=views.dashboard, post=views.add_event)), name='administration_dashboard'),
+    url(r'^event/(?P<pk>\d+)/delete/$', user_can_add_events(DeleteView.as_view(model=Event)), name='administration_event_delete'),
 )
