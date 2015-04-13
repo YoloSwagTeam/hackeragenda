1d1c3b420060718fa29a75e3ebb6dc6f642dd9ba
Laurent Peuch <cortex@worlddomination.be>
2015-04-13 10:09:28 +0200

    [enh] overload password_change view to change the template

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('authentication.views',
     url(r'^login/$', 'login', name='login'),
+    url(r'^password_change/$', 'password_change', name='password_change'),
     url(r'^', include("django.contrib.auth.urls")),
 )
--- authentication/views.py
+++ authentication/views.py
@@ -3,3 +3,7 @@ from django.contrib.auth import views as auth
 
 def login(request):
     return auth.login(request, template_name="registration/login.haml")
+
+
+def password_change(request):
+    return auth.password_change(request, template_name="registration/password_change_form.haml")
