4b2edb60521e71de388f73854f23d6dc61b40045
Laurent Peuch <cortex@worlddomination.be>
2015-06-02 10:27:34 +0200

    [enh] bolderplate for add_meetup script

---
+++ add_meetup.py
@@ -0,0 +1,35 @@
+from redbaron import RedBaron
+
+template = """
+@event_source(background_color="%(background_color)s", text_color="%(text_color)s", url="%(url)s", predefined_tags=[%(tags)s])
+def %(function_name)s():
+        \"\"\"%(description)s\"\"\"
+            return generic_meetup("%(meetup_name)s")
+""".strip()
+
+target_url = "http://www.meetup.com/Apprendre-a-programmer-un-site-WEB-debutant/"
+
+target_meetup_name = target_url.split("/")[-2]
+target = target_url.split("/")[-2].lower().replace("-", "_")
+
+red = RedBaron(open("agendas/be.py", "r").read())
+
+before = red("def", recursive=False)[0]
+
+for i in red("def", recursive=False):
+    if target < i.name:
+        break
+
+    before = i
+
+print i.name
+print
+print template % {
+    "background_color": "",
+    "text_color": "",
+    "url": target_url,
+    "tags": "",
+    "function_name": target,
+    "description": "",
+    "meetup_name": target_meetup_name,
+}
